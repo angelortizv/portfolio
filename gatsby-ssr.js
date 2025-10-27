6968d0a6c1a596ed6662ecc881d64d814d869ba8
Angelo Ortiz Vega <angelo@angelortizv.com>
2025-10-27 16:29:04 -0600

    fix,test: wrapRootElement on ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,11 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
- // You can delete this file if you're not using it
+import React from "react";
+import { LanguageProvider } from "./src/hooks/LanguageContext";
+
+export const wrapRootElement = ({ element }) => (
+  <LanguageProvider>{element}</LanguageProvider>
+);
+
+
